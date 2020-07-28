3e918f437211463836ba2210619be48577109e9d
kisstar <dwh.chn@foxmail.com>
2020-07-28 19:17:17 +0800

    feat: building the infrastructure of a web site

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.less'
+
+/* eslint-disable global-require */
+if (process.env.NODE_ENV !== 'production') {
+  // module.hot && module.hot.accept() /* 无法共存 */
+  require('./index.html')
+}
+/* eslint-enable global-require */
